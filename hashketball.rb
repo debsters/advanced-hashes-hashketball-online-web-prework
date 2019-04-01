ca2614164d73004544b759f00c40e539cea169ad
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-04-01 15:03:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -125,8 +125,14 @@ def num_points_scored(name)
   end
 end
 
-def shoe_size(game_hash)
-
+def shoe_size(name)
+    game_hash.each do |location, team_data|
+      team_data[:players].each do |player_name, player_stat|
+        if name == player_name
+          return player_stat[:shoe]
+        end
+      end
+    end    
 end
 
 def team_colors(game_hash)
